ce97c3f1523337ed42daf54d9dfece21fd24c242
l1f21bsse0372 <l1f21bsse0372@ucp.edu.pk>
2025-01-27 11:02:49 +0500

    Initial commit

---
+++ config.sh
@@ -0,0 +1,6 @@
+echo "Basic configurations"
+git config --local user.name "l1f21bsse0372"
+git config --local user.email "l1f21bsse0372@ucp.edu.pk"
+mkdir ./src ./styles
+touch .htmlhintrc.json .stylelintrc.json
+npm init -y 
